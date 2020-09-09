ec97866237792d02d14598bfa0f8f4d319d9ac98
vhaf123 <victor.aranaf92@gmail.com>
2020-09-08 23:31:06 -0500

    actualizacion

--- resources/views/manuales/laravel.blade.php
+++ resources/views/manuales/laravel.blade.php
@@ -8,6 +8,8 @@
 
     <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/styles/default.min.css">
 
+    <link rel="stylesheet" href="{{asset('css/manual.css')}}">
+
     <style>
 
         
@@ -81,89 +83,87 @@
 
 @section('content')
 
-    @include('layouts.partials.social-bar')
+@include('layouts.partials.social-bar')
 
-    <div class="container mt-4">
-        <div class="row">
+<div class="container mt-4">
+    <div class="row">
 
-            <div class="col-12">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-                      <li class="breadcrumb-item"><a href="{{route('laravel.index')}}">Documentación de Laravel 8 en español</a></li>
-                      <li class="breadcrumb-item active" aria-current="page">@if ($actual) {{$actual->name}} @else No hay ningún tema agregado @endif</li>
-                    </ol>
-                </nav>
-            </div>
-
-            <nav class="col-12 col-md-4 text-secondary mb-4">
-                <div class="card shadow">
-                    <div class="card-body">
-
-                        <h1 class="h3 text-center mb-3">LARAVEL 8</h1>
-
-                        <ul class="list-unstyled indice">
-
-                            @forelse ($manual->capitulos as $capitulo)
-            
-                                <li>
-                                    <h1 class="h6 font-weight-bold">{{$capitulo->name}}</h1>
-                
-                                    <ul class="subindice">
-                                        @foreach ($capitulo->temas as $tema)
-                                        
-                                            @if ($tema->status == 2)
-                                                <li @if ($tema->id == $actual->id) class="active" @endif>
-                                                    <h2 class = "h6">
-                                                        <a href="{{route('laravel.tema',  $tema)}}" class="text-secondary text-decoration-none">
-                                                            {{$tema->name}}
-                                                        </a>
-                                                    </h2>
-                                                </li>
-                                            @endif
-
-                                        @endforeach
-                                        
-                                    </ul>
-                
-                                </li>
-                
-                            @empty
-                                <div class="alert alert-danger" role="alert">
-                                    <strong>Aún no se ha agregado ningún tema</strong>
-                                </div>
-                            @endforelse
-                        </ul>
-                        
-
-                    </div>
-                </div>
+        <div class="col-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{route('laravel.index')}}">Documentación de Laravel 8 en español</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">@if ($actual) {{$actual->name}} @else No hay ningún tema agregado @endif</li>
+                </ol>
             </nav>
+        </div>
 
-            <div class="col-12 col-md-8">
-                <div class="card">
-                    <div class="card-body text-secondary">
+        <nav class="col-12 col-md-4 text-secondary mb-4">
+            <div class="card shadow">
+                <div class="card-body">
 
-                        @if ($actual)
-                            <h1 class="text-uppercase text-center">{{$actual->name}}</h1>
-                            <p>{{$actual->descripcion}}</p>
-                            {!!$actual->body!!}
+                    <h1 class="h3 text-center mb-3">LARAVEL 8</h1>
 
-                        @else
+                    <ul class="list-unstyled indice">
+
+                        @forelse ($manual->capitulos as $capitulo)
+        
+                            <li>
+                                <h1 class="h6 font-weight-bold">{{$capitulo->name}}</h1>
+            
+                                <ul class="subindice">
+                                    @foreach ($capitulo->temas as $tema)
+                                    
+                                        @if ($tema->status == 2)
+                                            <li @if ($tema->id == $actual->id) class="active" @endif>
+                                                <h2 class = "h6">
+                                                    <a href="{{route('laravel.tema',  $tema)}}" class="text-secondary text-decoration-none">
+                                                        {{$tema->name}}
+                                                    </a>
+                                                </h2>
+                                            </li>
+                                        @endif
+
+                                    @endforeach
+                                    
+                                </ul>
+            
+                            </li>
+            
+                        @empty
                             <div class="alert alert-danger" role="alert">
-                                <strong>No se ha agregado ningún tema aún</strong>
+                                <strong>Aún no se ha agregado ningún tema</strong>
                             </div>
-                        @endif
+                        @endforelse
+                    </ul>
+                    
 
-                    </div>
                 </div>
             </div>
+        </nav>
 
-            
+        <div class="col-12 col-md-8">
+            <div class="card">
+                <div class="card-body text-secondary">
+                    {{-- <h1 class="text-uppercase text-center">{{$actual->name}}</h1> --}}
+                    @if ($actual)
+                        <p>{{$actual->descripcion}}</p>
+                        {!!$actual->body!!}
+
+                    @else
+                        <div class="alert alert-danger" role="alert">
+                            <strong>No se ha agregado ningún tema aún</strong>
+                        </div>
+                    @endif
+
+                    
+                </div>
+            </div>
         </div>
-    </div>
 
-    
+        
+    </div>
+</div>
 
 @endsection
 
