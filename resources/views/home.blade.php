3f492828a529a417e168f742c82e06f6a33f5c3f
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:59:30 -0500

    cursos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -185,6 +185,20 @@
                         </div>
                     </div>
                 @endforelse
+
+                {{-- Publicidad --}}
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-format="fluid"
+                        data-ad-layout-key="-74+cn+3r-1o+36"
+                        data-ad-client="ca-pub-8456964757737909"
+                        data-ad-slot="1907367096"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
                 
             </div>
         
