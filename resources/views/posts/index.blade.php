a480552a6941a6d98fb8f5a4369d245c5bb9a1ab
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:41:28 -0500

    posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -44,21 +44,7 @@
 
         <div class="row">
             <div class="col-12 col-lg-8">
-                <div class="mb-0">
-                    
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-8456964757737909"
-                        data-ad-slot="6924406974"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-
-
+                
                 <div class="row">
 
                     @forelse ($posts as $post)
