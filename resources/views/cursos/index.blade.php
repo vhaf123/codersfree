b15600dabf00e7ed0ad9f44a542d7881a87cb13f
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:28:10 -0500

    posts

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -146,6 +146,23 @@
                         @include('cursos.partials.card-curso')
 
                     </div>
+
+
+                    <div class="col-12">
+                        {{-- Publicidad --}}
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Horizontal -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-8456964757737909"
+                            data-ad-slot="6924406974"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
+
                 @empty
                     <div class="col-12">
                         <div class="alert alert-primary text-center" role="alert">
