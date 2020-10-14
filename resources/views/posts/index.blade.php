3b47c314bfb49b797a4e2d71f5e3e3109f158052
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:11:59 -0500

    posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -54,6 +54,20 @@
                                 @include('posts.partials.card-post')
                             </div>
 
+                            <div class="col-12 mb-4">
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- Horizontal -->
+                                <ins class="adsbygoogle"
+                                    style="display:block"
+                                    data-ad-client="ca-pub-8456964757737909"
+                                    data-ad-slot="6924406974"
+                                    data-ad-format="auto"
+                                    data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </div>
+
                         @else
 
                             <div class="col-12 col-md-6 mb-4">
