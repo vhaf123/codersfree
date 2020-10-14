2c8250f99ba13bd14053e06a61933cc285d99712
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:33:39 -0500

    posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -44,6 +44,23 @@
         <div class="row">
             <div class="col-12 col-lg-8">
 
+                <div class="">
+                    {{-- Publicidad --}}
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- Horizontal -->
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-8456964757737909"
+                        data-ad-slot="6924406974"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+
+
+
                 <div class="row">
 
                     @forelse ($posts as $post)
@@ -53,22 +70,7 @@
                             <div class="col-12 mb-4">
                                 @include('posts.partials.card-post')
                             </div>
-
-                            <div class="col-12">
-                                {{-- Publicidad --}}
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                <!-- Horizontal -->
-                                <ins class="adsbygoogle"
-                                    style="display:block"
-                                    data-ad-client="ca-pub-8456964757737909"
-                                    data-ad-slot="6924406974"
-                                    data-ad-format="auto"
-                                    data-full-width-responsive="true"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
-
+                            
                         @else
 
                             <div class="col-12 col-md-6 mb-4">
