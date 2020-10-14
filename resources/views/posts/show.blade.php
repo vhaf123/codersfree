032a961ee22b9e0a9f9e5b0fb4d9691c1bce3c8f
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 15:47:40 -0500

    post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -170,6 +170,22 @@
                         <article class="card-body px-md-5">
                             <h1>{!!$post->name!!}</h1>
                             {!!$post->body!!}
+
+
+
+                            <div>
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <ins class="adsbygoogle"
+                                    style="display:block; text-align:center;"
+                                    data-ad-layout="in-article"
+                                    data-ad-format="fluid"
+                                    data-ad-client="ca-pub-8456964757737909"
+                                    data-ad-slot="8744238188"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </div>
+
                             
                         
                             {{-- Te puede interesar --}}
