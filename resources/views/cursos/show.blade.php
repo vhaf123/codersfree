5e033c9b77667d7e4952b5380b8943e767b6f2dc
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 19:23:51 -0500

    cursos

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -373,16 +373,18 @@
                     @forelse ($similares as $similar)
 
                         @if ($loop->last)
-                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                            <ins class="adsbygoogle"
-                                style="display:block"
-                                data-ad-format="fluid"
-                                data-ad-layout-key="-hc+o-31-8q+rb"
-                                data-ad-client="ca-pub-8456964757737909"
-                                data-ad-slot="5970912799"></ins>
-                            <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
+                            <div class="mb-3">
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <ins class="adsbygoogle"
+                                    style="display:block"
+                                    data-ad-format="fluid"
+                                    data-ad-layout-key="-hc+o-31-8q+rb"
+                                    data-ad-client="ca-pub-8456964757737909"
+                                    data-ad-slot="5970912799"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </div>
                         @endif
 
 
@@ -409,16 +411,19 @@
                         </article>
 
                         @if ($loop->first)
-                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                            <ins class="adsbygoogle"
-                                style="display:block"
-                                data-ad-format="fluid"
-                                data-ad-layout-key="-hc+o-31-8q+rb"
-                                data-ad-client="ca-pub-8456964757737909"
-                                data-ad-slot="5970912799"></ins>
-                            <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
+
+                            <div class="mb-3">
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <ins class="adsbygoogle"
+                                    style="display:block"
+                                    data-ad-format="fluid"
+                                    data-ad-layout-key="-hc+o-31-8q+rb"
+                                    data-ad-client="ca-pub-8456964757737909"
+                                    data-ad-slot="5970912799"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </div>
                         @endif
 
                     @empty
