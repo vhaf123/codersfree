e6f361ff257e39b867c06a10b1e971fec22a3c18
vhaf123 <victor.aranaf92@gmail.com>
2020-10-14 19:35:36 -0500

    cursos

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -427,17 +427,19 @@
                         @endif
 
                     @empty
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                        <!-- recomendacion_cuadrado -->
-                        <ins class="adsbygoogle"
-                            style="display:block"
-                            data-ad-client="ca-pub-8456964757737909"
-                            data-ad-slot="2003747628"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
+                        <div>
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- recomendacion_cuadrado -->
+                            <ins class="adsbygoogle"
+                                style="display:block"
+                                data-ad-client="ca-pub-8456964757737909"
+                                data-ad-slot="2003747628"
+                                data-ad-format="auto"
+                                data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
                     @endforelse
                 </aside>
             </div>
