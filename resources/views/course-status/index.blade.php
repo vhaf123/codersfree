126d4729df33e7fe842a5f5f8e9ab18aaefd234c
vhaf123 <victor.aranaf92@gmail.com>
2020-10-17 22:02:29 -0500

    anuncio

--- resources/views/course-status/index.blade.php
+++ resources/views/course-status/index.blade.php
@@ -117,7 +117,7 @@
                             </div>
                         </div>
 
-                        <div class="my-4 d-lg-none">
+                        <div class="my-4">
                             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                             <!-- Horizontal -->
                             <ins class="adsbygoogle"
