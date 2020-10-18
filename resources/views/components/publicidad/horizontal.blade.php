0891b07a83677af6e4655724bde38eb408b9a3a5
vhaf123 <victor.aranaf92@gmail.com>
2020-10-18 14:12:26 -0500

    publicidad

---
+++ resources/views/components/publicidad/horizontal.blade.php
@@ -0,0 +1,11 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Horizontal -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8456964757737909"
+     data-ad-slot="6924406974"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
