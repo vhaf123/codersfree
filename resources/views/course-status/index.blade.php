ec4dc724cb3790e0ef2bbe444ee9a0b7c12ca48b
vhaf123 <victor.aranaf92@gmail.com>
2020-10-17 21:56:58 -0500

    anuncio

--- resources/views/course-status/index.blade.php
+++ resources/views/course-status/index.blade.php
@@ -134,6 +134,23 @@
                 </div>
 
                 <aside class="col-12 col-lg-4 px-lg-4 indice">
+
+                    <div class="mb-4 d-none d-lg-block">
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Horizontal -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-8456964757737909"
+                            data-ad-slot="6924406974"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
+
+
+
                     <div class="card shadow">
                         <div class="card-body">
                             <h1 class="h4 text-center">
