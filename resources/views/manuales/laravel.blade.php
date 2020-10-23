3a2d7b8acc1e9c64f0032e504ba829846cd81bac
vhaf123 <victor.aranaf92@gmail.com>
2020-10-23 11:03:19 -0500

    publicidad

--- resources/views/manuales/laravel.blade.php
+++ resources/views/manuales/laravel.blade.php
@@ -152,8 +152,17 @@
                 </div>
             </div>
 
-            <div class="mb-4">
-                <x-publicidad.horizontal/>
+            <div class="mb-3">
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <ins class="adsbygoogle"
+                    style="display:block; text-align:center;"
+                    data-ad-layout="in-article"
+                    data-ad-format="fluid"
+                    data-ad-client="ca-pub-8456964757737909"
+                    data-ad-slot="8744238188"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
             </div>
         </aside>
 
